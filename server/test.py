63ea285296317002b7a8887ac6979a9759f8610e
Akuldeep Jakkula <akuldeepj@gmail.com>
2024-04-08 19:47:40 +0530

    restructured

--- server/test.py
+++ server/test.py
@@ -8,49 +8,9 @@ from urllib.parse import unquote
 from langchain import PromptTemplate
 from transcript import get_transcript
 from gemini_llm import summarize
-
+from llama_llm import summarize_llama, extract_data_website, split_text_chunks
 
 app=Flask(__name__)
-#1. Extract Data From the Website
-def extract_data_website(url):
-    loader=SeleniumURLLoader([url])
-    data=loader.load()
-    text=""
-    for page in data:
-        text +=page.page_content + " "
-        return text
-#2. Generate a Summary of the Text
-def summarize_llama(text):
-    text_chunks = split_text_chunks(text)
-
-    llm = LlamaCpp(
-    model_path="models/llama-2-7b-chat.Q4_0.gguf",
-    n_ctx=8128,
-    device="mps",
-    n_batch=256,
-    n_gpu_layers=32,
-    verbose=True,  # Verbose is required to pass to the callback manager
-)
-    prompt_template = """Write a concise summary of the following text delimited by triple backquotes.
-              Return your response in bullet points which covers the key points of the text.
-              ```{text}```
-              BULLET POINT SUMMARY:
-  """
-
-    prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
-
-    docs = [Document(page_content=t) for t in text_chunks]
-    chain=load_summarize_chain(llm=llm, chain_type='stuff', verbose=True,prompt=prompt)
-    summary = chain.run(docs)
-    return summary
-
-
-def split_text_chunks(text):
-    text_splitter=CharacterTextSplitter(separator='\n',
-                                        chunk_size=1000,
-                                        chunk_overlap=20)
-    text_chunks=text_splitter.split_text(text)
-    return text_chunks
 
 
 @app.route('/', methods=['GET', 'POST'])
